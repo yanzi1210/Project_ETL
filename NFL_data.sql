b458c72a5b8be588c6924610a2cf8decb7a8c575
Yan Xu <44480701+yanzi1210@users.noreply.github.com>
2019-05-12 17:38:00 -0500

    Delete NFL_data.sql

--- NFL_data.sql
+++
@@ -1,4 +0,0 @@
-USE sports_db;
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SELECT COLLEGE,COUNT(*) AS 'Number_of_Best_Players' 
-FROM NFL_data GROUP BY COLLEGE ORDER BY Number_of_Best_Players desc;
