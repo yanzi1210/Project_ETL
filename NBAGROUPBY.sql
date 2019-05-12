7d25f18bc26fd4344ed2d05decb5104b1c7b8852
Yan Xu <44480701+yanzi1210@users.noreply.github.com>
2019-05-12 17:38:10 -0500

    Delete NBAGROUPBY.sql

--- NBAGROUPBY.sql
+++
@@ -1,7 +0,0 @@
-USE sports_db;
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SELECT COLLEGE,YEAR,COUNT(*) AS 'Number_of_Best_Players' 
-FROM NBA_player_school GROUP BY COLLEGE, YEAR ORDER BY YEAR ASC;
-
-
-
